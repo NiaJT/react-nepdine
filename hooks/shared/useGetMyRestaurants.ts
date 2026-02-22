1bd570f267bc5096e55ac32543e359215b206d17
NiaJT <nirajthapa457@gmail.com>
2026-02-22 22:30:21 +0545

    Modified useGetMyRestaurants.ts

--- hooks/shared/useGetMyRestaurants.ts
+++ hooks/shared/useGetMyRestaurants.ts
@@ -1,4 +1,4 @@
-import { MyRestaurantResponse } from "@/app/(auth)/restaurants/page";
+import type { MyRestaurantResponse } from "@/pages/auth/Restaurants";
 import { axiosInstance } from "@/lib/axios.instance";
 import { useQuery } from "@tanstack/react-query";
 
